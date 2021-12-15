a284c2ad7a14bab467338b49853527f412a3924c
Mihai Răducanu <raducanu@gmail.com>
2021-12-15 14:18:06 +0200

    wip

--- src/components/Room/Room.jsx
+++ src/components/Room/Room.jsx
@@ -1,12 +1,25 @@
 import { inboxStream, outboxStream } from '@logic/socket.js'
-import { Button, CardActions, CardContent } from '@mui/material'
+import { Button } from '@mui/material'
 import React, { Component } from 'react'
 import { filter } from 'rxjs/operators'
 
 import * as S from './styled'
 
-const defaultState = { question: '', options: null, timeRemaining: null, buttonStates: [] }
-const buttonStateColors = { CORRECT: 'success', INCORRECT: 'error' }
+const defaultState = { question: '', options: null, timeRemaining: null, buttonStates: [null, null, null, null] }
+const buttonStateColors = {
+  CORRECT: 'success',
+  INCORRECT: 'error',
+  KNOWN_INCORRECT: 'error',
+  UNKNWON: 'primary',
+  KNOWN_CORRECT: 'success',
+}
+const buttonStateVariants = {
+  CORRECT: 'contained',
+  INCORRECT: 'contained',
+  KNOWN_INCORRECT: 'outlined',
+  UNKNOWN: 'outlined',
+  KNOWN_CORRECT: 'outlined',
+}
 
 class Room extends Component {
   constructor(...args) {
@@ -18,11 +31,21 @@ class Room extends Component {
     this.setState({ ...defaultState })
   }
 
-  updateAnswer(payload) {
+  updateWithSolution(payload) {
     const index = this.state.options.indexOf(payload.answer)
-    const buttonStates = [...this.state.buttonStates]
-    buttonStates[index] = payload.status
-    this.setState({ buttonStates })
+    this.setState({
+      buttonStates: this.state.buttonStates.map((_, i) =>
+        i === index ? payload.status : payload.status === 'CORRECT' ? 'KNOWN_INCORRECT' : 'UNKNOWN'
+      ),
+    })
+  }
+
+  updateWithResults(payload) {
+    const index = this.state.options.indexOf(payload.correctAnswer)
+    if (this.state.buttonStates[index] !== 'CORRECT')
+      this.setState({
+        buttonStates: this.state.buttonStates.map((buttonState, i) => (i === index ? 'KNOWN_CORRECT' : buttonState)),
+      })
   }
 
   componentDidMount() {
@@ -33,7 +56,8 @@ class Room extends Component {
         if (event.type === 'GAME/OPTIONS') this.setState({ options: event.payload })
         if (event.type === 'GAME/UPDATE')
           this.setState({ question: event.payload.question, options: event.payload.options })
-        if (event.type === 'GAME/SOLUTION') this.updateAnswer(event.payload)
+        if (event.type === 'GAME/SOLUTION') this.updateWithSolution(event.payload)
+        if (event.type === 'GAME/RESULTS') this.updateWithResults(event.payload)
       }),
     })
   }
@@ -48,17 +72,18 @@ class Room extends Component {
         <S.QuestionCard>
           <S.QuestionCardContent>{this.state.question}</S.QuestionCardContent>
           {this.state.options ? (
-            <CardActions>
+            <S.QuestionCardOptions>
               {this.state.options.map((answer, i) => (
                 <Button
                   key={answer}
                   color={buttonStateColors[this.state.buttonStates[i]] || 'primary'}
+                  variant={buttonStateVariants[this.state.buttonStates[i]] || 'contained'}
                   onMouseDown={() => outboxStream.next({ type: 'GAME/ANSWER', payload: answer })}
                 >
                   {answer}
                 </Button>
               ))}
-            </CardActions>
+            </S.QuestionCardOptions>
           ) : null}
         </S.QuestionCard>
       </S.Room>
--- src/components/Room/styled.js
+++ src/components/Room/styled.js
@@ -13,12 +13,31 @@ export const QuestionCard = styled(Card)`
   margin: 0 auto;
   left: 0;
   right: 0;
-  width: 640px;
-  height: 300px;
+  width: calc(100vw - 32px);
+  max-width: 600px;
 `
 
 export const QuestionCardContent = styled(CardContent)`
-  font-size: 32px;
+  font-size: 1.5rem;
   font-weight: bold;
   text-align: center;
+  min-height: 150px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+export const QuestionCardOptions = styled(CardContent)`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  & > .MuiButton-root {
+    width: calc(50% - 8px);
+    height: 64px;
+    line-height: 1.2;
+  }
+  & > .MuiButton-root:nth-of-type(3),
+  & > .MuiButton-root:nth-of-type(4) {
+    margin-top: 16px;
+  }
 `
